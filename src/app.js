9810ea267100e73ba90129cfcf43c6999ab36ac0
Lovekush40 <l.kprajapati9611@gmail.com>
2026-03-30 19:17:04 +0530

    updating mistakes

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import "./config/passport.js"
 const app = express();
 
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || "*",,
+  origin: process.env.CORS_ORIGIN || "*",
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));

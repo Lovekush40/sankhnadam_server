00d04d78b16e324e1e91c36da88e34557b020919
Lovekush40 <l.kprajapati9611@gmail.com>
2026-04-03 08:20:25 +0530

    Adding My-bookings

--- src/controllers/booking.controller.js
+++ src/controllers/booking.controller.js
@@ -3,7 +3,7 @@ import ApiResponse from "../utils/ApiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
 
 const getUserBookings = asyncHandler(async(req, res) => {
-    const myBooking = Booking.find({userId: req.user._id }).sort({createdAt: -1})
+    const myBooking = await Booking.find({userId: req.user._id }).sort({createdAt: -1})
 
     console.log("My bookings", myBooking)
 
